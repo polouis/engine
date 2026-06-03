50b259c94603d94e61e330dc57d211b7501e2f42
Pierre-Olivier Louis <polouis@gmail.com>
2026-06-03 13:33:29 +0200

    Added ressource management

--- engine.go
+++ engine.go
@@ -10,23 +10,46 @@ import (
 )
 
 type Context struct {
-	W *World
-	b backend.Backend
+	W  *World
+	RM *RessourceManager
+	b  backend.Backend
 }
 
 func New(bt types.BackendType) *Context {
 	switch bt {
 	case types.SDL:
-		return &Context{W: NewWorld(), b: &backendsdl.BackendSDL{}}
+		return &Context{W: NewWorld(), RM: NewRessourceManager(), b: &backendsdl.BackendSDL{}}
 	case types.Dummy:
-		return &Context{W: NewWorld(), b: &backenddummy.BackendDummy{}}
+		return &Context{W: NewWorld(), RM: NewRessourceManager(), b: &backenddummy.BackendDummy{}}
 	default:
 		panic(fmt.Sprintf("Cannot instanciate unknown backend '%v'", bt))
 	}
 }
 
-func Run(ctx *Context, initCallback func(), updateCallback func(uint64), releaseCallback func()) error {
-	return ctx.b.Run(initCallback, updateCallback, releaseCallback)
+func initCtxBindingCallback(ctx *Context, initCallback func(*Context)) func() {
+	return func() {
+		initCallback(ctx)
+	}
+}
+
+func updateCtxBindingCallback(ctx *Context, updateCallback func(*Context, uint64)) func(uint64) {
+	return func(delta uint64) {
+		updateCallback(ctx, delta)
+	}
+}
+
+func releaseCtxBindingCallback(ctx *Context, releaseCallback func(*Context)) func() {
+	return func() {
+		releaseCallback(ctx)
+	}
+}
+
+func Run(ctx *Context, initCallback func(*Context), updateCallback func(*Context, uint64), releaseCallback func(*Context)) error {
+	return ctx.b.Run(
+		initCtxBindingCallback(ctx, initCallback),
+		updateCtxBindingCallback(ctx, updateCallback),
+		releaseCtxBindingCallback(ctx, releaseCallback),
+	)
 }
 
 func GetKeyState(ctx *Context, k types.KeyType) bool {
---
+++ ressource.go
@@ -0,0 +1,139 @@
+package engine
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/polouis/engine/types"
+)
+
+// /*
+//   - Need a table describing the VB when shared between multiple entities
+//   - - mesh id/name
+//   - -
+//  */
+
+type RessourceVertex struct {
+	X float32 `json:"x"`
+	Y float32 `json:"y"`
+	Z float32 `json:"z"`
+	R uint8   `json:"r"`
+	G uint8   `json:"g"`
+	B uint8   `json:"b"`
+	A uint8   `json:"a"`
+}
+
+type RessourceMesh struct {
+	Id       string            `json:"id"`
+	Vertices []RessourceVertex `json:"vertices"`
+}
+
+type RessourceMeshId struct {
+	Id string `json:"id"`
+}
+
+type RessourceName struct {
+	Name string `json:"name"`
+}
+
+type RessourceTransform struct {
+	X float32 `json:"x"`
+	Y float32 `json:"y"`
+	Z float32 `json:"z"`
+}
+
+type rawComponentEntry struct {
+	Type  string          `json:"type"`
+	Value json.RawMessage `json:"value"`
+}
+
+type RessourceEntity struct {
+	Components []rawComponentEntry `json:"components"`
+}
+
+type RessourceManager struct {
+	meshRessources map[string]RessourceMesh
+}
+
+func NewRessourceManager() *RessourceManager {
+	return &RessourceManager{meshRessources: make(map[string]RessourceMesh)}
+}
+
+func (rm *RessourceManager) LoadMesh(dat []byte) {
+	var mesh RessourceMesh
+	if err := json.Unmarshal(dat, &mesh); err != nil {
+		panic(err)
+	}
+	_, exists := rm.meshRessources[mesh.Id]
+	if exists {
+		panic(fmt.Errorf("Mesh Id %s already exists", mesh.Id))
+	}
+	rm.meshRessources[mesh.Id] = mesh
+}
+
+func (rm *RessourceManager) LoadEntity(dat []byte) *RessourceEntity {
+	var ent RessourceEntity
+	if err := json.Unmarshal(dat, &ent); err != nil {
+		panic(err)
+	}
+
+	for _, c := range ent.Components {
+		fmt.Println(c.Type)
+		switch c.Type {
+		case "mesh":
+			var rsMesh RessourceMeshId
+			if err := json.Unmarshal(c.Value, &rsMesh); err != nil {
+				panic(err)
+			}
+			fmt.Println(rsMesh.Id)
+		case "name":
+			var rsName RessourceName
+			if err := json.Unmarshal(c.Value, &rsName); err != nil {
+				panic(err)
+			}
+			fmt.Println(rsName.Name)
+		case "transform":
+			var rsTransform RessourceTransform
+			if err := json.Unmarshal(c.Value, &rsTransform); err != nil {
+				panic(err)
+			}
+			fmt.Println(rsTransform)
+		}
+	}
+
+	return &ent
+}
+
+func (rm *RessourceManager) Spawn(ctx *Context, ent *RessourceEntity) EntityID {
+	e := ctx.W.NewEntity()
+	for _, c := range ent.Components {
+		switch c.Type {
+		case "name":
+			var r RessourceName
+			json.Unmarshal(c.Value, &r)
+			ctx.W.NameStore.Upsert(e, NameComponent{Name: r.Name})
+		case "transform":
+			var r RessourceTransform
+			json.Unmarshal(c.Value, &r)
+			ctx.W.TransformStore.Upsert(e, TransformComponent{
+				Position: Vector3{X: r.X, Y: r.Y, Z: r.Z},
+			})
+		case "mesh":
+			var r RessourceMeshId
+			json.Unmarshal(c.Value, &r)
+			mesh := rm.meshRessources[r.Id] // resolve from cache
+			var verts []types.PositionColorVertex = make([]types.PositionColorVertex, len(mesh.Vertices))
+			for i, v := range mesh.Vertices {
+				verts[i].X = v.X
+				verts[i].Y = v.Y
+				verts[i].Z = v.Z
+				verts[i].R = v.R
+				verts[i].G = v.G
+				verts[i].B = v.B
+				verts[i].A = v.A
+			}
+			ctx.W.MeshStore.Upsert(e, NewMeshComponent(ctx, verts))
+		}
+	}
+	return e
+}
