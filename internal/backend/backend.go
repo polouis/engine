ea3e897eee3686ad925fe7660bacb62b7f61e976
Pierre-Olivier Louis <polouis@gmail.com>
2026-07-29 18:28:29 +0200

    Cleaned code

--- internal/backend/backend.go
+++ internal/backend/backend.go
@@ -9,11 +9,13 @@ type Mesh2dUniform struct {
 
 type Backend interface {
 	Run(initCallback func(), updateCallback func(uint64), releaseCallback func()) error
+
 	NewVertexBuffer([]types.PositionColorVertex) VertexBuffer
+	PushVertexUniformData(u Mesh2dUniform)
+	Draw(vb VertexBuffer)
+	Release(vb VertexBuffer)
+
 	GetKeyState(k types.KeyType) bool
 	GetButtonState(b types.ButtonType) bool
-	Draw(vb VertexBuffer)
-	PushVertexUniformData(u Mesh2dUniform)
-	Release(vb VertexBuffer)
 }
 type VertexBuffer interface{}
